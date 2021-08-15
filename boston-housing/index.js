e4d4afa93e4f4eee7d9cf23664f0586b6cec4cc5
Haesun Park <haesunrpark@gmail.com>
2021-08-15 19:40:52 +0900

    보스턴 예제 추가

---
+++ boston-housing/index.js
@@ -0,0 +1,198 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+import * as tf from '@tensorflow/tfjs';
+import * as tfvis from '@tensorflow/tfjs-vis';
+
+import {BostonHousingDataset, featureDescriptions} from './data';
+import * as normalization from './normalization';
+import * as ui from './ui';
+
+// Some hyperparameters for model training.
+const NUM_EPOCHS = 200;
+const BATCH_SIZE = 40;
+const LEARNING_RATE = 0.01;
+
+const bostonData = new BostonHousingDataset();
+const tensors = {};
+
+// Convert loaded data into tensors and creates normalized versions of the
+// features.
+export function arraysToTensors() {
+  tensors.rawTrainFeatures = tf.tensor2d(bostonData.trainFeatures);
+  tensors.trainTarget = tf.tensor2d(bostonData.trainTarget);
+  tensors.rawTestFeatures = tf.tensor2d(bostonData.testFeatures);
+  tensors.testTarget = tf.tensor2d(bostonData.testTarget);
+  // Normalize mean and standard deviation of data.
+  let {dataMean, dataStd} =
+      normalization.determineMeanAndStddev(tensors.rawTrainFeatures);
+
+  tensors.trainFeatures = normalization.normalizeTensor(
+      tensors.rawTrainFeatures, dataMean, dataStd);
+  tensors.testFeatures =
+      normalization.normalizeTensor(tensors.rawTestFeatures, dataMean, dataStd);
+};
+
+/**
+ * Builds and returns Linear Regression Model.
+ *
+ * @returns {tf.Sequential} The linear regression model.
+ */
+export function linearRegressionModel() {
+  const model = tf.sequential();
+  model.add(tf.layers.dense({inputShape: [bostonData.numFeatures], units: 1}));
+
+  model.summary();
+  return model;
+};
+
+/**
+ * Builds and returns Multi Layer Perceptron Regression Model
+ * with 1 hidden layers, each with 10 units activated by sigmoid.
+ *
+ * @returns {tf.Sequential} The multi layer perceptron regression model.
+ */
+export function multiLayerPerceptronRegressionModel1Hidden() {
+  const model = tf.sequential();
+  model.add(tf.layers.dense({
+    inputShape: [bostonData.numFeatures],
+    units: 50,
+    activation: 'sigmoid',
+    kernelInitializer: 'leCunNormal'
+  }));
+  model.add(tf.layers.dense({units: 1}));
+
+  model.summary();
+  return model;
+};
+
+/**
+ * Builds and returns Multi Layer Perceptron Regression Model
+ * with 2 hidden layers, each with 10 units activated by sigmoid.
+ *
+ * @returns {tf.Sequential} The multi layer perceptron regression mode  l.
+ */
+export function multiLayerPerceptronRegressionModel2Hidden() {
+  const model = tf.sequential();
+  model.add(tf.layers.dense({
+    inputShape: [bostonData.numFeatures],
+    units: 50,
+    activation: 'sigmoid',
+    kernelInitializer: 'leCunNormal'
+  }));
+  model.add(tf.layers.dense(
+      {units: 50, activation: 'sigmoid', kernelInitializer: 'leCunNormal'}));
+  model.add(tf.layers.dense({units: 1}));
+
+  model.summary();
+  return model;
+};
+
+
+/**
+ * Describe the current linear weights for a human to read.
+ *
+ * @param {Array} kernel Array of floats of length 12.  One value per feature.
+ * @returns {List} List of objects, each with a string feature name, and value
+ *     feature weight.
+ */
+export function describeKernelElements(kernel) {
+  tf.util.assert(
+      kernel.length == 12,
+      `kernel must be a array of length 12, got ${kernel.length}`);
+  const outList = [];
+  for (let idx = 0; idx < kernel.length; idx++) {
+    outList.push({description: featureDescriptions[idx], value: kernel[idx]});
+  }
+  return outList;
+}
+
+/**
+ * Compiles `model` and trains it using the train data and runs model against
+ * test data. Issues a callback to update the UI after each epcoh.
+ *
+ * @param {tf.Sequential} model Model to be trained.
+ * @param {boolean} weightsIllustration Whether to print info about the learned
+ *  weights.
+ */
+export async function run(model, modelName, weightsIllustration) {
+  model.compile(
+      {optimizer: tf.train.sgd(LEARNING_RATE), loss: 'meanSquaredError'});
+
+  let trainLogs = [];
+  const container = document.querySelector(`#${modelName} .chart`);
+
+  ui.updateStatus('훈련 과정을 시작합니다...');
+  await model.fit(tensors.trainFeatures, tensors.trainTarget, {
+    batchSize: BATCH_SIZE,
+    epochs: NUM_EPOCHS,
+    validationSplit: 0.2,
+    callbacks: {
+      onEpochEnd: async (epoch, logs) => {
+        await ui.updateModelStatus(
+            `에포크 ${NUM_EPOCHS}번 중 ${epoch + 1}번째 완료.`, modelName);
+        trainLogs.push(logs);
+        tfvis.show.history(container, trainLogs, ['loss', 'val_loss'],
+            {'xLabel':'반복(에포크)','yLabel':'손실'})
+
+        if (weightsIllustration) {
+          model.layers[0].getWeights()[0].data().then(kernelAsArr => {
+            const weightsList = describeKernelElements(kernelAsArr);
+            ui.updateWeightDescription(weightsList);
+          });
+        }
+      }
+    }
+  });
+
+  ui.updateStatus('테스트 데이터에서 평가합니다...');
+  const result = model.evaluate(
+      tensors.testFeatures, tensors.testTarget, {batchSize: BATCH_SIZE});
+  const testLoss = result.dataSync()[0];
+
+  const trainLoss = trainLogs[trainLogs.length - 1].loss;
+  const valLoss = trainLogs[trainLogs.length - 1].val_loss;
+  await ui.updateModelStatus(
+      `훈련 세트 최종 손실: ${trainLoss.toFixed(4)}\n` +
+      `검증 세트 최종 손실: ${valLoss.toFixed(4)}\n` +
+      `테스트 세트 손실: ${testLoss.toFixed(4)}`,
+      modelName);
+};
+
+export function computeBaseline() {
+  const avgPrice = tensors.trainTarget.mean();
+  console.log(`평균 가격: ${avgPrice.dataSync()}`);
+  const baseline = tensors.testTarget.sub(avgPrice).square().mean();
+  console.log(`기준 손실: ${baseline.dataSync()}`);
+  const baselineMsg = `기준 손실(meanSquaredError): ${
+      baseline.dataSync()[0].toFixed(2)}`;
+  ui.updateBaselineStatus(baselineMsg);
+};
+
+document.addEventListener('DOMContentLoaded', async () => {
+  await bostonData.loadData();
+  ui.updateStatus('데이터가 로드되었고 텐서로 변환합니다');
+  arraysToTensors();
+  ui.updateStatus(
+      '데이터가 텐서로 변환되었습니다..\n' +
+      '훈련 버튼을 눌러 시작하세요.');
+  // TODO Explain what baseline loss is. How it is being computed in this
+  // Instance
+  ui.updateBaselineStatus('기준 손실을 추정합니다.');
+  computeBaseline();
+  await ui.setup();
+}, false);
---
+++ boston-housing/normalization.js
@@ -0,0 +1,47 @@
+/**
+ * @license
+ * Copyright 2018 Google LLC. All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * =============================================================================
+ */
+
+/**
+ * Calculates the mean and standard deviation of each column of a data array.
+ *
+ * @param {Tensor2d} data: 각 열의 평균과 표준 편차를 독립적으로 계산하기 위한 데이터셋
+  *
+ * @returns {Object}: 각 열의 평균과 표준 편차를 1d 텐서로 포함하고 있는 객체
+ */
+export function determineMeanAndStddev(data) {
+  const dataMean = data.mean(0);
+  // TODO(bileschi): Simplify when and if tf.var / tf.std added to the API.
+  const diffFromMean = data.sub(dataMean);
+  const squaredDiffFromMean = diffFromMean.square();
+  const variance = squaredDiffFromMean.mean(0);
+  const dataStd = variance.sqrt();
+  return {dataMean, dataStd};
+}
+
+/**
+ * 평균과 표준 편차가 주어지면 평균을 빼고 표준 편차로 나누어 정규화합니다.
+ *
+ * @param {Tensor2d} data: 정규화할 데이터. 크기: [batch, numFeatures].
+ * @param {Tensor1d} dataMean: 데이터의 평균. 크기 [numFeatures].
+ * @param {Tensor1d} dataStd: 데이터의 표준 편차. 크기 [numFeatures]
+ *
+ * @returns {Tensor2d}: data와 동일한 크기의 텐서이지만,
+ * 각 열은 평균이 0이고 단위 표준 편차를 가지도록 정규화되어 있습니다.
+ */
+export function normalizeTensor(data, dataMean, dataStd) {
+  return data.sub(dataMean).div(dataStd);
+}
